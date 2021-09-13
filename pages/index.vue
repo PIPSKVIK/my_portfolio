c513992edfe8a5e1942150e4e43b2c803a8f7bbc
Александр Милованов <aleksandrmilovanov@MacBook-Air-Aleksandr.local>
2021-09-13 22:22:38 +0300

    create

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-block">Будущее портфолио фотографа.</div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+}
+</script>
+
+<style lang="scss" scoped>
+.main-block {
+  height: 300px;
+  background: purple;
+  @include md-desktop {
+    background: green;
+  }
+}
+</style>
